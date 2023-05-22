ababc98e6866427a88d8c5c97b4e9e5b4941e3a6
HieuTH <hieth@powersoft.vn>
2023-05-22 16:01:51 +0700

    update new source

--- hieuth/src/app/app.component.ts
+++ hieuth/src/app/app.component.ts
@@ -21,10 +21,10 @@ export class AppComponent implements OnInit {
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle("Muffeez Khalid | MEAN Stack Developer | MERN Stack Developer | Software Engineer");
+    this.titleService.setTitle("Trương Hữu Hiếu | Full Stack Developer | Full Stack Developer | Software Engineer");
     this.metaService.addTags([
-      { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development , Software Engineer, software, developer' },
-      { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
+      { name: 'keywords', content: 'fullstack, Full Stack Developer , Full Stack Development , Software Engineer, software, developer' },
+      { name: 'description', content: 'As a software engineer with expertise in both front-end and back-end, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
     ]);
     
     AOS.init();
--- hieuth/src/app/services/language/language.service.ts
+++ hieuth/src/app/services/language/language.service.ts
@@ -7,7 +7,7 @@ import { Location } from '@angular/common';
 })
 export class LanguageService {
 
-  language: "es" | "en";
+  language: "vie" | "en";
 
   constructor(
     public translateService: TranslateService,
@@ -15,7 +15,7 @@ export class LanguageService {
   ) {}
 
   initLanguage(){
-    this.translateService.addLangs(["en", "es"])
+    this.translateService.addLangs(["en", "vie"])
     let language = navigator.language || (navigator as any).userLanguage;
     language = "en"
     this.translateService.setDefaultLang(language)
